2450090597cce8212edcbfe67d370150405ac1ef
paul-bouton <bouton.paul@outlook.fr>
2024-03-13 21:54:09 +0100

    FIX : Add static and use snake_case

--- pedaling_mgt/pedaling_mgt.c
+++ pedaling_mgt/pedaling_mgt.c
@@ -9,24 +9,23 @@
 #include <stdbool.h>
 
 //Cyclical buffer of size 13.
-//We allocate one extra location (13 instaed of 12)
+//We allocate one extra location (13 instead of 12)
 //to be able to compute the average speed, keeping track
 //of the current magnet previous activation value.
 #define n_magnet 12u
 static uint32_t cyclic_buff[n_magnet+1u]={0u};
 static uint8_t buff_pos = n_magnet; //Start at the end of the buffer
+static bool is_buff_full = false;
 
-uint8_t getPrevPos(void){
+static uint8_t get_prev_pos(void){
     return (buff_pos == 0u) ? n_magnet
                             : buff_pos-1u;
 }
 
-uint8_t getNextPos(void){
+static uint8_t get_next_pos(void){
     return (buff_pos == n_magnet) ? 0u
                                   : buff_pos+1u;
 }
-
-static bool is_buff_full = false;
 
 /**** Public API ****/
 
@@ -37,7 +36,7 @@ float get_pedaling_speed(void){
         //set to n_magnet at init
         return 0.0f;
     }
-    const float delta_t = cyclic_buff[buff_pos] - cyclic_buff[getPrevPos()];
+    const float delta_t = cyclic_buff[buff_pos] - cyclic_buff[get_prev_pos()];
     return 1e6f / (n_magnet*delta_t); //Get the speed in rotation / seconds
 }
 
@@ -48,13 +47,13 @@ float get_average_pedaling_speed(void){
         return 0.0f;
     }
     //Since the buffer is of size n_magnet+1,
-    //cyclic_buff[getNextPos()] gives the previous value of cyclic_buff[buff_pos]
-    const float delta_t = cyclic_buff[buff_pos] - cyclic_buff[getNextPos()];
+    //cyclic_buff[get_next_pos()] gives the previous value of cyclic_buff[buff_pos]
+    const float delta_t = cyclic_buff[buff_pos] - cyclic_buff[get_next_pos()];
     return 1e6f / delta_t; //Get the speed in rotation / seconds
 }
 
 void new_magnet_cb(void){
-    buff_pos = getNextPos();
+    buff_pos = get_next_pos();
     cyclic_buff[buff_pos] = get_timestamp();
     if(!is_buff_full && (buff_pos == n_magnet)){
         is_buff_full = true;
